567ddf4756911b27c05794553c9370b711c0487a
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-15 21:55:01 +0000

    adding name functionailty

--- client.js
+++ client.js
@@ -1,11 +1,19 @@
 const net = require('net');
 
+process.stdin.resume();
+
 const connect = function() {
   const conn = net.createConnection({ 
     host: '192.168.88.45',
     port: 50541
   });
   conn.setEncoding('utf8'); 
+
+  conn.on('connect', () => {
+    console.log('success')
+    conn.write(`Name: MCC`);
+  });
+
 
   conn.on('data', (data) => {
     console.log(data)
